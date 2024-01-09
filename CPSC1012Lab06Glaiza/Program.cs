3d67fc69ab42d8d2a7f7bfa1ad847531285c85c0
Glaiza Loren Malit <122337351+gmalit@users.noreply.github.com>
2024-01-09 15:04:36 -0700

    Add project files.

---
+++ CPSC1012Lab06Glaiza/Program.cs
@@ -0,0 +1,23 @@
+﻿using CPSC1012Lab06Glaiza;
+using System.Diagnostics.Metrics;
+
+
+Stock stock = new Stock();
+string symbol = "";
+
+Console.Write("Enter the stock name: ");
+stock.Name = Console.ReadLine();
+Console.Write("Enter the stock symbol: ");
+stock.Symbol = Console.ReadLine();
+Console.Write($"Enter the {stock.Name} ({stock.Symbol}) previous closing price: ");
+stock.PreviousClosingPrice = double.Parse(Console.ReadLine());
+Console.Write($"Enter the {stock.Name} ({stock.Symbol}) current stock price: ");
+stock.CurrentPrice = double.Parse(Console.ReadLine());
+if (stock.CurrentPrice > stock.PreviousClosingPrice)
+{
+    symbol = "+";
+}
+
+Console.WriteLine($"\n\n{stock.Name} ({stock.Symbol}):Current price - ${stock.CurrentPrice}, {symbol}{stock.ChangePrice()} ({symbol}{stock.ChangePercent()}%)");
+
+
---
+++ CPSC1012Lab06Glaiza/Stock.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPSC1012Lab06Glaiza
+{
+    internal class Stock
+    {
+        // Properties - auto implemented
+        public double CurrentPrice { get; set; }
+        public string Name { get; set; }
+        public double PreviousClosingPrice { get; set; }
+        public string Symbol { get; set; }
+
+
+        // Methods
+        public double ChangePercent()
+        {
+            double changePercent;
+            changePercent = Math.Round((CurrentPrice - PreviousClosingPrice) / PreviousClosingPrice * 100, 2);
+            return changePercent;
+        }
+
+        public double ChangePrice()
+        {
+            double changePrice;
+            int currentLength = CurrentPrice.ToString().Substring(CurrentPrice.ToString().IndexOf(".") + 1).Length;
+            int previousLength = PreviousClosingPrice.ToString().Substring(PreviousClosingPrice.ToString().IndexOf(".") + 1).Length;
+            int decimalPlaces;
+            if (currentLength > previousLength)
+            {
+                decimalPlaces = currentLength;
+            }
+            else
+            {
+                decimalPlaces = previousLength;
+            }
+            changePrice = CurrentPrice - PreviousClosingPrice;
+            return Math.Round(changePrice, decimalPlaces);
+        }
+
+
+        public Stock()
+        {
+            Name = "";
+            Symbol = "";
+            PreviousClosingPrice = 0;
+            CurrentPrice = 0;
+        }
+
+        public Stock(string name, string symbol, double previousClosingPrice, double currentPrice)
+        {
+            CurrentPrice = currentPrice;
+            Name = name;
+            PreviousClosingPrice = previousClosingPrice;
+            Symbol = symbol;
+        }
+    }
+}
